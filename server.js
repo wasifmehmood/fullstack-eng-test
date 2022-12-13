9298c082fe0198765896acad7bd830748b3b893f
Wasif <wasifmehmood20@gmail.com>
2022-12-13 18:44:56 +0500

    code structure improved

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const http = require('http');
 const routes = require('./routes');
-
-const hostname = '127.0.0.1'; // - optional
-const port = 3000;
+const { port, host } = require('./config/config');
 
 const server = http.createServer((req, res) => routes(req, res));
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}/`);
 });
